bed664c08ea139481c4e10f03b37bd5c1286f626
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-16 12:30:14 +0200

    Merge conflict with my self

--- setupEnv.sh
+++ setupEnv.sh
@@ -1,5 +1,9 @@
+# SPecify the folder structure you want
+
 FOLDER_1="srcs"
 FOLDER_2="mand"
+
+# setup the env
 
 touch $FOLDER_1/.env
 echo "DOMAIN_NAME=$USER.42.fr" > $FOLDER_1/.env
@@ -9,3 +13,15 @@ echo "DB_NAME=wordpress" >> $FOLDER_1/.env
 echo "DB_ROOT=rootpass" >> $FOLDER_1/.env
 echo "DB_USER=wpuser" >> $FOLDER_1/.env
 echo "DB_PASS=wppass" >> $FOLDER_1/.env
+
+# setup the Certificates
+
+cd ~/$FOLDER_1/$FOLDER_2/tools/
+mkcert kvebers.42.fr
+mv kvebers.42.fr-key.pem kvebers.42.fr.key
+mv kvebers.42.fr.pem kvebers.42.fr.crt
+
+
+
+
+
