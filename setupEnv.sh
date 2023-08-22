8cce9f437f2838c87efbbaffd328e844b1103ad3
kvebers <kvebers@student.42heilbronn.de>
2023-08-22 08:05:26 -0500

    Update on how things are lunched

--- setupEnv.sh
+++ setupEnv.sh
@@ -9,10 +9,11 @@ touch $FOLDER_1/.env
 echo "DOMAIN_NAME=$USER.42.fr" > $FOLDER_1/.env
 echo "CERT_=./$FOLDER_2/tools/$USER.42.fr.crt" >> $FOLDER_1/.env
 echo "KEY_=./$FOLDER_2/tools/$USER.42.fr.key" >> $FOLDER_1/.env
-echo "MYSQL_ROOT_PASSWORD=123" >> $FOLDER_1/.env
-echo "MYSQL_USER=123" >> $FOLDER_1/.env
-echo "MYSQL_PASSWORD=123" >> $FOLDER_1/.env
-echo "MYSQL_DATABASE=wordpress" >> $FOLDER_1/.env
+echo "DB_NAME=wordpress" >> $FOLDER_1/.env
+echo "ROOT_PASS=123" >> $FOLDER_1/.env
+echo "ROOT_USER=noproblem" >> $FOLDER_1/.env
+echo "NORM_USER=123" >> $FOLDER_1/.env
+echo "NORM_PASS=problem" >> $FOLDER_1/.env
 
 # setup the Certificates
 mkcert $USER.42.fr
