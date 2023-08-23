2dfadce8bdb251350168892207bb0a790550e445
kvebers <kvebers@student.42heilbronn.de>
2023-08-23 05:24:29 -0500

    Updates in the mkcert certificate auto setup fun

--- setupEnv.sh
+++ setupEnv.sh
@@ -17,6 +17,7 @@ echo "NORM_PASS=problem" >> $FOLDER_1/.env
 
 # setup the Certificates
 mkcert $USER.42.fr
+mkcert -install # fixing some wierd issue that could occure for some setups
 mv $USER.42.fr-key.pem ./$FOLDER_1/$FOLDER_2/nginx/cert/$USER.42.fr.key
 mv $USER.42.fr.pem ./$FOLDER_1/$FOLDER_2/nginx/cert/$USER.42.fr.crt
 
