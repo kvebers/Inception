3532111ae2846aa8637a7cfd232c6dd9c6cad57f
kvebers <karlisvilhelms@gmail.com>
2023-09-06 11:16:06 +0200

    Fix in the bash file

--- setupEnv.sh
+++ setupEnv.sh
@@ -10,11 +10,11 @@ echo "DOMAIN_NAME=$USER.42.fr" > $FOLDER_1/.env
 echo "CERT_=./$FOLDER_2/tools/$USER.42.fr.crt" >> $FOLDER_1/.env
 echo "KEY_=./$FOLDER_2/tools/$USER.42.fr.key" >> $FOLDER_1/.env
 echo "DB_NAME=wordpress" >> $FOLDER_1/.env
-echo "ROOT_PASS=123" >> $FOLDER_1/.env
+echo "ROOT_PASS=onetwothree" >> $FOLDER_1/.env
 echo "ROOT_USER=noproblem" >> $FOLDER_1/.env
-echo "NORM_USER=123" >> $FOLDER_1/.env
-echo "NORM_PASS=problem" >> $FOLDER_1/.env
-echo "MYSQL_SOCKET=/var/run/mysqld/"
+echo "NORM_USER=problem" >> $FOLDER_1/.env
+echo "NORM_PASS=problemuser" >> $FOLDER_1/.env
+echo "MYSQL_SOCKET=/var/run/mysqld/" >> $FOLDER_1/.env
 
 # setup the Certificates
 
