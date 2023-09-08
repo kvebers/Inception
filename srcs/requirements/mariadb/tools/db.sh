ecc2436668a2cb708a89ce2ee284af36c2770273
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-08 19:53:55 +0200

    Seems to be working

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -1,45 +1,15 @@
-#!/bin/sh
+#!bin/sh
 
+cat > problem.sql <<EOF
+CREATE DATABASE IF NOT EXISTS {MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '{MYSQL_USER}' IDENTIFIED BY '{MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON {MYSQL_DATABASE}.* TO '{MYSQL_USER}';
+FLUSH PRIVILEGES;
+EOF
 
-mkdir -p /run/mysqld && chown -R mysql:mysql /run/mysqld
-mysqld_safe &
-for i in {30..0}; do
-    if echo 'SELECT 1' | mysql &> /dev/null; then
-        break
-    fi
-    sleep 1
-done
+replace "{MYSQL_DATABASE}" "$MYSQL_DATABASE" -- problem.sql
+replace "{MYSQL_USER}" "$MYSQL_USER" -- problem.sql
+replace "{MYSQL_PASSWORD}" "$MYSQL_PASSWORD" -- problem.sql
+replace "{MYSQL_ROOT_PASSWORD}" "$MYSQL_ROOT_PASSWORD" -- problem.sql
 
-if [ "$i" = 0 ]; then
-    echo >&2 "MariaDB did not start"
-    exit 1
-fi
-
-if ! command -v mysql &> /dev/null; then
-    apk update
-    apk add mariadb mariadb-client
-fi
-
-existing_db=$(mysql -e "SHOW DATABASES LIKE '$DB_NAME';" | grep "$DB_NAME")
-if [ -n "$existing_db" ]; then
-    exit 1
-fi
-
-mysql -e "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-
-existing_root_user=$(mysql -e "SELECT User FROM mysql.user WHERE User='$ROOT_USER';" | grep "$ROOT_USER")
-if [ -n "$existing_root_user" ]; then
-    echo "User '$ROOT_USER' already exists."
-else
-    mysql -e "GRANT ALL ON *.* TO '$ROOT_USER'@'localhost' IDENTIFIED BY '$ROOT_PASS' WITH GRANT OPTION;"
-fi
-
-existing_normal_user=$(mysql -e "SELECT User FROM mysql.user WHERE User='$NORM_USER';" | grep "$NORM_USER")
-if [ -z "$existing_normal_user" ]; then
-    mysql -e "CREATE USER '$NORM_USER'@'%' IDENTIFIED BY '$NORM_PASS';"
-    mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$NORM_USER'@'%';"
-    mysql -e "FLUSH PRIVILEGES;"
-fi
-
-wait
-
+exec mariadbd --no-defaults --user=root --datadir=/var/lib/mysql --init-file=/problem.sql
