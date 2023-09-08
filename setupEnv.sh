52f9dc11a190540b0c821c84a03cc06f47fb6487
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-08 18:27:02 +0200

    Small fixes

--- setupEnv.sh
+++ setupEnv.sh
@@ -9,13 +9,7 @@ touch $FOLDER_1/.env
 echo "DOMAIN_NAME=$USER.42.fr" > $FOLDER_1/.env
 echo "CERT_=/etc/ssl/certs/kvebers.42.fr.crt" >> $FOLDER_1/.env
 echo "KEY_=/etc/ssl/private/kvebers.42.fr.key" >> $FOLDER_1/.env
-echo "DB_NAME=wordpress" >> $FOLDER_1/.env
-echo "ROOT_PASS=onetwothree" >> $FOLDER_1/.env
-echo "ROOT_USER=noproblem" >> $FOLDER_1/.env
-echo "NORM_USER=problem" >> $FOLDER_1/.env
-echo "NORM_PASS=problemuser" >> $FOLDER_1/.env
-# echo "MYSQL_SOCKET=/var/run/mysqld/mysqld.sock" >> $FOLDER_1/.env
-
-# setup the Certificates
-
-docker network create ok
+echo "MYSQL_DATABASE=problemdb"
+echo "MYSQL_ROOT_PASSWORD=noproblem"
+echo "MYSQL_USER=problemuser"
+echo "MYSQL_PASSWORD=onetwothree"
